ae5284884192aac57d8e75d73861be07469746da
EndZy21 <endzy1771@gmail.com>
2024-09-25 17:24:49 +0300

    created a conculator, created a sheet (with the last number inverted) and divided the sheets into paired and unpaired

---
+++ урок 3.py
@@ -0,0 +1,33 @@
+# num_1 =  float(input("Ведите первое число: "))
+# num_2 =  float(input("Ведите первое число: "))
+# operator = input("Ведите математическое уровнение: ")
+# if operator == "+":
+#    result_1 = num_1 + num_2
+#    print("Результат: ")
+#    print(result_1)
+# elif operator == "-" :
+#     result_2 = num_1 - num_2
+#     print("Результат: ")
+#     print(result_2)
+# elif operator == "*":
+#     result_3 = num_1 * num_2
+#     print("Результат: ")
+#     print(result_3)
+# elif operator  == "/" and num_2 == 0 :
+#     print("На ноль делить нельзя!")
+# elif  operator == "/" and num_2 != 0 :
+#     result_4 = num_1 / num_2
+#     print("Результат: ")
+#     print(result_4)
+
+
+
+
+# list_1 = list(range(0, 10, 2))
+# print(list_1)
+# list_1 = list(range(1, 10, 2))
+# print(list_1)
+
+# nambers = [1, 2, 3, 4,]
+# nambers.insert(0,nambers.pop() )
+# print(nambers)
